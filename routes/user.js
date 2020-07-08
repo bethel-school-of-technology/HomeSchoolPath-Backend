94bf3e0a017a0e5d04efedfe5e8df3d14f474553
Milton Lash <iblash@live.com>
2020-07-08 00:42:34 -0500

    fixed syntax error in route file

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,6 @@ const { read, publicProfile, update, photo } = require("../controllers/user");
 router.get("/user/profile", requireSignin, authMiddleware, read);
 router.get("/user/:username", publicProfile);
 router.put("/user/update", requireSignin, authMiddleware, update);
-router.get("user/photo/:username", photo);
+router.get("/user/photo/:username", photo);
 
 module.exports = router;
